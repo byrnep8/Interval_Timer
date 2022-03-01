d94c7f29301e3f963b5e49a205784b1bf1726f0d
byrnep8 <pbyrne98.pb@gmail.com>
2022-03-01 19:48:52 +0000

    Updating repo with working programming of Shift Register

--- Shift_Register_Programming/Shift_Register_Programming.ino
+++ Shift_Register_Programming/Shift_Register_Programming.ino
@@ -1,6 +1,6 @@
 /*
  * Programming the HC595 Shift Register
- * 
+ *
  * Use of the shift regioster will allow us to save pins of the atmega for the connecting of the 7 segment display
  * For shift register operation:
  *  Connect pins 11 and 12 (SHCP, STCP) together, these are clocks for the 2 internal register blocks
@@ -13,7 +13,7 @@
  *  PIND1:  MR
  *  PIND2:  OE
  *  PIND3:  DS
- * 
+ *
  */
  // Imports of header files for the ATMEGA register mapping as well as the standard C libraries
 #include <avr/io.h>
@@ -21,36 +21,45 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <util/delay.h>
+// #include <util/delay.h>
 #include <stdbool.h>
 #include <string.h>
 
 void toggle_clk();
-void update_shift_register_output(int []digits, int arr_size);
+void update_shift_register_output(int *digits, int arr_size);
 void reset_shift_register();
 
 /*
  * Toggle the clk Pin
+ * PORTD Pin 0
  */
 void toggle_clk()
 {
-  PIND0 = 1;
-  PIND0 = 0;
+  PORTD |= 0b00000001;
+  PORTD &= 0b11111110;
 }
 
 /*
  * Function to update the register output with array passed
  * Turn output on afterwards, pull OE low
+ *  PORTD PIN 3
  */
-void update_shift_register_output(int []digits, int arr_size)
+void update_shift_register_output(int *digits, int arr_size)
 {
   // Change DS to array, pulse clk
   for(int i=0; i<arr_size; i++)
   {
-    PIND3=digits[i];
+    // PIND3=digits[i];
+    if( digits[i]>0){
+      PORTD |= 0b00001000;
+    }
+    else{
+      PORTD &= 0b11110111;
+    }
     toggle_clk();
   }
-  PIND2=0;
+  // PIND2=0;
+  PORTD &= 0b11111011;
   toggle_clk();
 }
 
@@ -60,10 +69,14 @@ void update_shift_register_output(int []digits, int arr_size)
 void reset_shift_register()
 {
   // Pull MR low, pulse the clock, pull high
-  PIND1=0;
-  PIND2=1;
+  // PIND1=0;
+  PORTD &= 0b11111101;
+  // PIND2=1;
+  PORTD |= 0b00000100;
+ 
   toggle_clk();
-  PIND1=1;
+  // PIND1=1;
+  PORTD |= 0b00000010;
 }
 
 // Main function
@@ -85,5 +98,31 @@ int main(){
   int dig_7[] = {0,0,0,0,1,1,1};
   int dig_8[] = {1,1,1,1,1,1,1};
   int dig_9[] = {1,1,0,1,1,1,1};
-  int arr_dig[] ={dig_0,dig_1,dig_2,dig_3,dig_4,dig_5,dig_6,dig_7,dig_8,dig_9};
+//  int arr_dig[] = {&dig_9, &dig_8, &dig_7, &dig_6, &dig_5, &dig_4, &dig_3, &dig_2, &dig_1, &dig_0};
+  // Array of arrays for the digits of each number
+  int* arr_dig[10] = {dig_0,dig_1,dig_2,dig_3,dig_4,dig_5,dig_6,dig_7,dig_8,dig_9};
+ 
+//  int arr_dig[10][7] = {
+//    {0,1,1,1,1,1,1},
+//    {0,0,0,0,1,1,0},
+//    {1,0,1,1,0,1,1},
+//    {1,0,0,1,1,1,1},
+//    {1,1,0,0,1,1,0},
+//    {1,1,0,1,1,0,1},
+//    {1,1,1,1,1,0,1},
+//    {0,0,0,0,1,1,1},
+//    {1,1,1,1,1,1,1},
+//    {1,1,0,1,1,1,1}
+//  };
+  reset_shift_register();
+  int pattern1[] = {0,0,0,0,0,0,1};
+  // update_shift_register_output(arr_dig[1], 7);
+
+  for( int i = 0; i < 10; i++ ){
+    update_shift_register_output(arr_dig[i], 7);
+    // delay(500);
+    // reset_shift_register();
+    // delay(500);
+  }
+ 
 }
